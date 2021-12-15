6938c61ecfef0edc31f6813c348bfc936e457858
LifeofLight <78275747+LifeofLight@users.noreply.github.com>
2021-12-15 11:46:49 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,55 +1,63 @@
-import React from 'react'
-import { useState} from 'react';
-import {Text,SafeAreaView,StyleSheet,TextInput,Button, View} from 'react-native'
+//12/8
+// import React from 'react'
+// import { Touchable } from 'react-native';
+// import ImageWithTextInput from './components/ImageWithTextInput';
+// import TextInputReview from './components/TextInputReview';
+// import ButtonExample from './components/ButtonExample';
+// import TextInputPractice1 from './components/TextInputPractice1'
+// import TouchableExample from './components/TouchableExample';
+// import TouchablePractice from './components/TouchablePractice';npx
 
-
-
-
-
-const App=()=>{
-  
-
-  const[inputValue,setInputvalue]=useState('')
-
+// const App=()=>{
  
-  const checkValue =()=>{
-    if(isNaN(inputValue)){
-      alert("IT is not a number")
-    }else{
-      alert("it's number")
-    }
-  }
 
-  return(
-    <SafeAreaView style={{flex:1}}>
-       <View style={styles.container}>
-        <TextInput
-        placeholder ="EnterText"
-        style={styles.textinput}
-        onChangeText={(inputValue)=>{setInputvalue(inputValue)}}
-        />
-        <Button
-        title="Check Value Is Number or Not"
-        onPress={checkValue}
-        />
-       </View>
+//   return(
+//     <TextInputReview/>
+//     <ImageWithTextInput/>
+//     <ButtonExample/>
+//     <TextInputPractice1/>
+//     <TouchableExample/>
+//     <TouchablePractice/>
+//     )
+// }
 
-    </SafeAreaView>
+//   export default App;
+
+//12/15  recap
+//import React from 'react'
+// import { StyleSheet, Text, View,Button } from 'react-native'
+// import Logo from'./components/Logo'
+// import Footer from './components/Footer'
+// const App = () => {
+//   const showData =()=>{
+//     alert('hello code')
+//   }
+//   return (
+//     <View>
+//     <Logo/>
+//     <Text>Hello React native</Text>
+//     <Button title='Click' onPress={showData}/>
+//     <Footer/>
+//     </View>
+    
+//   )
+// }
+
+// export default App
+// const styles = StyleSheet.create({})
+
+
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+//import AlertExample from './components/AlertExample'
+import ModelExample from './components/ModelExample'
+const App = () => {
+  return (
+    //<AlertExample/>
+    <ModelExample/>
   )
 }
 
-const styles = StyleSheet.create({
-   container: {
-   flex: 1,
-   alignItems:'center',
-   marginTop:60
-   },
-   textinput: {
-   textAlign :'center',
-   width:'70%',
-   marginBottom:10,
-   borderColor :'red'
-   }
-   });
-   export default App;
+export default App
 
+const styles = StyleSheet.create({})
