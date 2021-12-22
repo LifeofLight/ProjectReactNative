1ebbb573edf17cb05a1544392f2d168426102ed7
LifeofLight <78275747+LifeofLight@users.noreply.github.com>
2021-12-22 11:45:37 +0700

    Add files via upload
    webView

--- App.js
+++ App.js
@@ -46,18 +46,58 @@
 // export default App
 // const styles = StyleSheet.create({})
 
-
+/*
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 //import AlertExample from './components/AlertExample'
-import ModelExample from './components/ModelExample'
+//import ModelExample from './components/ModelExample'
+//import WebViewExample from './components/WebViewExample'
+import WebViewLoading from './components/WebViewLoading'
 const App = () => {
   return (
     //<AlertExample/>
-    <ModelExample/>
-  )
+    //<ModelExample/>
+   //<WebViewExample/>
+   <WebViewLoading/> 
+   )
 }
 
 export default App
 
 const styles = StyleSheet.create({})
+*/
+
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native'
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreens from "./screens/HomeScreens"; 
+import AboutScreen from "./screens/AboutScreen";
+
+const Stack =createNativeStackNavigator();
+
+const App=()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#135790',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+        <Stack.Screen name="Home" component={HomeScreens} 
+        options={{title:'หน้าหลัก'}}/>
+        <Stack.Screen name="About" component={AboutScreen} 
+        options={{title:'เกี่ยวกับ'}}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
