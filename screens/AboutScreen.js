e2a8ac2b0b428e3a6bfef1ce10b40af241978d1b
LifeofLight <78275747+LifeofLight@users.noreply.github.com>
2022-01-05 11:25:11 +0700

    1/5/2022 Firstpage/Secondpage/Home/AboutScreen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Button } from 'react-native'
 
-const AboutScreen = () => {
+//const AboutScreen = ({navigation}) => {
+const AboutScreen = ({route}) => {
+    const {email}=route.params;
+
     return (
         <View style={styles.container}>
             <Text>เกี่ยวกับ?_?</Text>
+            <Text>Email: {email}</Text>
+            {/* <Button
+                title='Go to About Page again....'
+                onPress={()=>navigation.push('About')}
+            />
+            <Button
+                title='Go to Home Page'
+                onPress={()=>navigation.push('Home')}
+
+            />
+            <Button
+                title='Go back'
+                onPress={()=>navigation.goBack()}
+
+            />
+            <Button
+                title='Go back to first screen in stack'
+                onPress={()=>navigation.popToTop()}
+            /> */}
+            
         </View>
     )
 }
